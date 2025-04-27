79c0f9fd8de5e46afc586e57ace7e42fb6ac98a9
Veilza <veilzakinsemi@gmail.com>
2025-04-27 18:03:33 -0500

    Update timeoutDuration back to 60 seconds

--- scripts/main.js
+++ scripts/main.js
@@ -57,8 +57,7 @@ class PlayerApprovalSystem {
   static currentApprovals = new Map()
 
   // Timer in milliseconds
-  // Set to 10 seconds for testing purposes, will be switched to 60 on full release
-  static timeoutDuration = 10000000
+  static timeoutDuration = 60 * 1000
 
   // Various nullable variables
   static approvalUI = null
